4b05c2e2e8cae3f6aab9fa6f0edf1e36811c4685
Avimitin <avimitin@gmail.com>
2021-12-27 21:18:03 +0800

    new,core: implement commit title parser
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,13 +1,55 @@
-use subprocess::{Exec, Redirection};
 use anyhow::{Context, Result};
+use regex::Regex;
+use subprocess::{Exec, Redirection};
 
 pub fn run() -> Result<()> {
-    let out = Exec::cmd("git").args(&["log", "--oneline", "--pretty=\"%s (%h)\""])
+    let out = Exec::cmd("git")
+        .args(&["log", "--oneline", "--pretty=\"%s\""])
         .stdout(Redirection::Pipe)
         .capture()
-        .with_context(|| { "fail to execute git command" })?
+        .with_context(|| "fail to execute git command")?
         .stdout_str();
-    println!("{}", out);
+    for line in out.lines() {
+        let cmt = CommitTitle::new(line);
+        println!("orig: {}\n{:#?}", line, cmt);
+    }
 
     Ok(())
+}
+
+// CommitTitle contains semantic version information from commit message
+#[allow(dead_code)]
+#[derive(Debug)]
+struct CommitTitle {
+    prefix: String,
+    component: Option<String>,
+    summary: String,
+    is_breaking: bool,
+}
+
+impl CommitTitle {
+    pub fn new(title: &str) -> Option<CommitTitle> {
+        let re = Regex::new(r"(^[a-zA-Z]{3})([,|!]?)([a-zA-Z/]*): (.+)").unwrap();
+        let cap = re.captures(title)?;
+
+        // TODO: This should be improve
+        Some(CommitTitle {
+            prefix: cap.get(1)?.as_str().to_string(),
+            is_breaking: cap.get(2)?.as_str() == "!",
+            component: cap.get(3).map(|text| text.as_str().to_string()),
+            summary: cap.get(4)?.as_str().to_string(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_regex() {
+        use super::CommitTitle;
+        assert!(CommitTitle::new("rew,core: remove useless pretty arg").is_some());
+        assert!(CommitTitle::new("rew,keymap/neogit: update neogit and fugitive keymap").is_some());
+        assert!(CommitTitle::new("doc: update readme").is_some());
+        assert!(CommitTitle::new("rew!plugins: remove famiu/nvim-reload").is_some());
+    }
 }
