669d61fa1603704a8b6d4620599a1e1541eea395
Avimitin <avimitin@gmail.com>
2021-12-29 17:47:12 +0800

    rwt,core: update regex rule
    Add rule to match commit hash.
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -21,6 +21,7 @@ pub fn run() -> Result<()> {
 #[allow(dead_code)]
 #[derive(Debug)]
 struct CommitTitle {
+    hash: String,
     prefix: String,
     component: Option<String>,
     summary: String,
@@ -29,15 +30,17 @@ struct CommitTitle {
 
 impl CommitTitle {
     pub fn new(title: &str) -> Option<CommitTitle> {
-        let re = Regex::new(r"(^[a-zA-Z]{3})([,|!]?)([a-zA-Z/]*): (.+)").unwrap();
+        let rule = r"\(([a-zA-Z0-9]+)\) ([a-zA-Z]{3})([,|!]?)([a-zA-Z/]*): (.+)";
+        let re = Regex::new(rule).unwrap();
         let cap = re.captures(title)?;
 
         // TODO: This should be improve
         Some(CommitTitle {
-            prefix: cap.get(1)?.as_str().to_string(),
-            is_breaking: cap.get(2)?.as_str() == "!",
-            component: cap.get(3).map(|text| text.as_str().to_string()),
-            summary: cap.get(4)?.as_str().to_string(),
+            hash: cap.get(1)?.as_str().to_string(),
+            prefix: cap.get(2)?.as_str().to_string(),
+            is_breaking: cap.get(3)?.as_str() == "!",
+            component: cap.get(4).map(|text| text.as_str().to_string()),
+            summary: cap.get(5)?.as_str().to_string(),
         })
     }
 }
@@ -47,9 +50,14 @@ mod test {
     #[test]
     fn test_regex() {
         use super::CommitTitle;
-        assert!(CommitTitle::new("rew,core: remove useless pretty arg").is_some());
-        assert!(CommitTitle::new("rew,keymap/neogit: update neogit and fugitive keymap").is_some());
-        assert!(CommitTitle::new("doc: update readme").is_some());
-        assert!(CommitTitle::new("rew!plugins: remove famiu/nvim-reload").is_some());
+        let should_work = |expect: &str| -> bool {
+            let ct = CommitTitle::new(expect);
+            println!("{:?}", ct);
+            ct.is_some()
+        };
+        assert!(should_work("(4b05c2e) new,core: implement commit title parser"));
+        assert!(should_work("(e0fbc13) rew,core: remove useless pretty arg"));
+        assert!(should_work("(8eee8e5) new: initiate changelog generator"));
+        assert!(should_work("(adad53h) rew!plugins: remove famiu/nvim-reload"));
     }
 }
